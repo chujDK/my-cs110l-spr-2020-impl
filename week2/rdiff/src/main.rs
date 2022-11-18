7ed83d9eddec9a5eff24a707422c4d2ec1f683f8
chujDK <1768485949@qq.com>
2022-11-18 21:49:02 +0800

    add: rdiff

--- week2/rdiff/src/main.rs
+++ week2/rdiff/src/main.rs
@@ -7,30 +7,66 @@ use std::process;
 pub mod grid;
 
 /// Reads the file at the supplied path, and returns a vector of strings.
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let file = File::open(filename)?;
+    let mut lines = vec![];
+    for line in io::BufReader::new(file).lines() {
+        let line = line?;
+        lines.push(line);
+    }
+
+    Ok(lines)
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn lcs(seq1: &Vec<String>, seq2: &Vec<String>) -> Grid {
     // Note: Feel free to use unwrap() in this code, as long as you're basically certain it'll
     // never happen. Conceptually, unwrap() is justified here, because there's not really any error
     // condition you're watching out for (i.e. as long as your code is written correctly, nothing
     // external can go wrong that we would want to handle in higher-level functions). The unwrap()
     // calls act like having asserts in C code, i.e. as guards against programming error.
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let mut common = Grid::new(seq1.len() + 1, seq2.len() + 1);
+
+    seq1.iter().enumerate().for_each(|(i, l)| {
+        seq2.iter().enumerate().for_each(|(j, r)| {
+            if l == r {
+                common
+                    .set(i + 1, j + 1, common.get(i, j).unwrap() + 1)
+                    .unwrap();
+            } else {
+                common
+                    .set(
+                        i + 1,
+                        j + 1,
+                        std::cmp::max(common.get(i + 1, j).unwrap(), common.get(i, j + 1).unwrap()),
+                    )
+                    .unwrap();
+            }
+        })
+    });
+
+    common
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
-fn print_diff(lcs_table: &Grid, lines1: &Vec<String>, lines2: &Vec<String>, i: usize, j: usize) {
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+fn print_diff(
+    lcs_table: &Grid,
+    lines1: &Vec<String>,
+    lines2: &Vec<String>,
+    i: usize,
+    j: usize,
+    prefix: usize,
+) {
+    if i > 0 && j > 0 && lines1[i - 1] == lines2[j - 1] {
+        print_diff(&lcs_table, &lines1, &lines2, i - 1, j - 1, prefix);
+        println!("{:prefix$}:   {}", i, lines1[i - 1], prefix = prefix);
+    } else if j > 0 && (i == 0 || lcs_table.get(i, j - 1) >= lcs_table.get(i - 1, j)) {
+        print_diff(&lcs_table, &lines1, &lines2, i, j - 1, prefix);
+        println!("{:prefix$}: > {}", j, lines2[j - 1], prefix = prefix);
+    } else if i > 0 && (j == 0 || lcs_table.get(i, j - 1) <= lcs_table.get(i - 1, j)) {
+        print_diff(&lcs_table, &lines1, &lines2, i - 1, j, prefix);
+        println!("{:prefix$}: < {}", i, lines1[i - 1], prefix = prefix);
+    }
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -40,8 +76,22 @@ fn main() {
     let filename1 = &args[1];
     let filename2 = &args[2];
 
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let file_lines1 =
+        read_file_lines(filename1).expect(&format!("failed to read file `{}'", filename1));
+    let file_lines2 =
+        read_file_lines(filename2).expect(&format!("failed to read file `{}'", filename2));
+
+    print_diff(
+        &lcs(&file_lines1, &file_lines2),
+        &file_lines1,
+        &file_lines2,
+        file_lines1.len(),
+        file_lines2.len(),
+        std::cmp::max(
+            (file_lines1.len() as f64).log10() as usize + 1,
+            (file_lines2.len() as f64).log10() as usize + 1,
+        ),
+    );
 }
 
 #[cfg(test)]
