d182f9686bd833cdc53f7ae6b5c698d5ce59722f
chujDK <1768485949@qq.com>
2022-11-20 13:31:45 +0800

    add: week3 part2 make linklist generic

--- week3/linked_list/src/linked_list.rs
+++ week3/linked_list/src/linked_list.rs
@@ -1,60 +1,67 @@
-use std::fmt;
+use std::fmt::{self, Display};
 use std::option::Option;
 
-pub struct LinkedList {
-    head: Option<Box<Node>>,
+type NodeNext<T> = Option<Box<Node<T>>>;
+
+pub struct LinkedList<T> {
+    head: NodeNext<T>,
     size: usize,
 }
 
-struct Node {
-    value: u32,
-    next: Option<Box<Node>>,
+struct Node<T> {
+    value: T,
+    next: NodeNext<T>,
 }
 
-impl Node {
-    pub fn new(value: u32, next: Option<Box<Node>>) -> Node {
-        Node {value: value, next: next}
+impl<T> Node<T> {
+    pub fn new(value: T, next: Option<Box<Node<T>>>) -> Node<T> {
+        Node {
+            value: value,
+            next: next,
+        }
     }
 }
 
-impl LinkedList {
-    pub fn new() -> LinkedList {
-        LinkedList {head: None, size: 0}
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
+        LinkedList {
+            head: None,
+            size: 0,
+        }
     }
-    
+
     pub fn get_size(&self) -> usize {
         self.size
     }
-    
+
     pub fn is_empty(&self) -> bool {
         self.get_size() == 0
     }
-    
-    pub fn push_front(&mut self, value: u32) {
-        let new_node: Box<Node> = Box::new(Node::new(value, self.head.take()));
+
+    pub fn push_front(&mut self, value: T) {
+        let new_node: Box<Node<T>> = Box::new(Node::new(value, self.head.take()));
         self.head = Some(new_node);
         self.size += 1;
     }
-    
-    pub fn pop_front(&mut self) -> Option<u32> {
-        let node: Box<Node> = self.head.take()?;
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        let node: Box<Node<T>> = self.head.take()?;
         self.head = node.next;
         self.size -= 1;
         Some(node.value)
     }
 }
 
-
-impl fmt::Display for LinkedList {
+impl<T: Display> fmt::Display for LinkedList<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut current: &Option<Box<Node>> = &self.head;
+        let mut current: &NodeNext<T> = &self.head;
         let mut result = String::new();
         loop {
             match current {
                 Some(node) => {
                     result = format!("{} {}", result, node.value);
                     current = &node.next;
-                },
+                }
                 None => break,
             }
         }
@@ -62,7 +69,7 @@ impl fmt::Display for LinkedList {
     }
 }
 
-impl Drop for LinkedList {
+impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
         let mut current = self.head.take();
         while let Some(mut node) = current {
@@ -70,6 +77,3 @@ impl Drop for LinkedList {
         }
     }
 }
-
-
-
